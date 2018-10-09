1685f92d352c23f3d68f488a9686a14c89ec9d1d
Plasma <nplusplus538@gmail.com>
2018-10-09 12:00:44 +0900

    YEP_MessageCoreのバグ修正パッチ公開

---
+++ plugins/DarkPlasma_YEP_MessageCorePatch.js
@@ -0,0 +1,38 @@
+// DarkPlasma_YEP_MessageCorePatch
+// Copyright (c) 2018 DarkPlasma
+// This software is released under the MIT license.
+// http://opensource.org/licenses/mit-license.php
+
+/*:
+ * @plugindesc YEP_MessageCoreのバグを修正する
+ * @author DarkPlasma
+ * @license MIT
+ * 
+ * @help
+ * YEP_MessageCore.js（1.19）の以下のバグを修正します
+ * - 名前ボックス表示中に戦闘に入ると、背景に名前ボックスが残ったままになる
+ */
+
+(function(){
+  'use strict';
+  var pluginName = 'DarkPlasma_YEP_MessageCorePatch';
+
+  var _SceneMap_snapForBattleBackground = Scene_Map.prototype.snapForBattleBackground;
+  Scene_Map.prototype.snapForBattleBackground = function() {
+    if (this.isNameWindowVisible()) {
+      this._messageWindow._nameWindow.visible = false;
+    }
+    _SceneMap_snapForBattleBackground.call(this);
+    if (this.hasNameWindow()) {
+      this._messageWindow._nameWindow.visible = true;
+    }
+  };
+
+  Scene_Map.prototype.hasNameWindow = function () {
+    return this._messageWindow && this._messageWindow._nameWindow;
+  };
+
+  Scene_Map.prototype.isNameWindowVisible = function () {
+    return this.hasNameWindow() && this._messageWindow._nameWindow.visible;
+  };
+})();
