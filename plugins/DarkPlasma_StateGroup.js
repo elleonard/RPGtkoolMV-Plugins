93c3293c9bd64d0ad2dd2e0ec5813eb2cb33a04d
plasma <librarian256@gmail.com>
2021-08-15 17:13:14 +0900

    1.1.0 ステートに対して複数グループ指定

--- plugins/DarkPlasma_StateGroup.js
+++ plugins/DarkPlasma_StateGroup.js
@@ -4,6 +4,7 @@
 // http://opensource.org/licenses/mit-license.php
 
 /**
+ * 2021/08/15 1.1.0 ステートに対してグループ複数を指定する機能を追加
  * 2019/08/05 1.0.0 公開
  */
 
@@ -14,38 +15,60 @@
  * 
  * @help
  *   ステートをグルーピングして優先度付します。
- *   同じグループに属するステートは重ねがけできず、同じグループの優先度の高いステートで上書きされます。
+ *   同じグループに属するステートは重ねがけできず、
+ *   同じグループの優先度の高いステートで上書きされます。
  * 
  *   ステートのメモ欄に以下のように記述してください。
  * 
- *   <StateGroup:（グループ名）> グループ名は任意の文字列です。毒 や Poison など、グループに名前をつけてください。
- *   <StatePriority:x> xは数値です。数値が大きいほど優先度が高くなります。
+ *   <StateGroup:（グループ名）>
+ *     グループ名は任意の文字列です。
+ *     毒 や Poison など、グループに名前をつけてください。
+ *     グループはカンマ区切りで複数指定できます。
+ *   <StatePriority:x>
+ *     xは数値です。数値が大きいほど優先度が高くなります。
  */
 (function () {
   'use strict';
 
-  var _extractMetadata = DataManager.extractMetadata;
+  const _DataManager_extractMetadata = DataManager.extractMetadata;
   DataManager.extractMetadata = function (data) {
-    _extractMetadata.call(this, data);
-    if (data.meta.StateGroup) {
-      data.stateGroup = String(data.meta.StateGroup);
-      data.statePriority = Number(data.meta.StatePriority || 0);
+    _DataManager_extractMetadata.call(this, data);
+    if (this.isState(data)) {
+      if (data.meta.StateGroup) {
+        data.stateGroup = String(data.meta.StateGroup).split(',').map(groupName => String(groupName).trim());
+        data.statePriority = Number(data.meta.StatePriority || 0);
+      } else {
+        data.stateGroup = [];
+      }
     }
   };
 
-  var _GameBattler_addState = Game_Battler.prototype.addState;
+  DataManager.isState = function (item) {
+    return item && $dataStates &&  $dataStates.includes(item);
+  };
+
+  const _Game_Action_testItemEffect = Game_Action.prototype.testItemEffect;
+  Game_Action.prototype.testItemEffect = function(target, effect) {
+    const result = _Game_Action_testItemEffect.call(this, target, effect);
+    if (effect.code === Game_Action.EFFECT_ADD_STATE) {
+      return result && !target.isHighPriorityStateGroupAffected(effect.dataId);
+    }
+    return result;
+  };
+
+  const _Game_Battler_addState = Game_Battler.prototype.addState;
   Game_Battler.prototype.addState = function (stateId) {
     // 優先度の低い同グループステートにかかっている場合は上書きする
     if (this.isLowPriorityStateGroupAffected(stateId)) {
-      this.eraseState(this.affectedSameGroupState(stateId).id);
+      this.affectedSameGroupStates(stateId).forEach(state => this.eraseState(state.id));
     }
-    _GameBattler_addState.call(this, stateId);
+    _Game_Battler_addState.call(this, stateId);
   };
 
-  var _GameBattler_isStateAddable = Game_Battler.prototype.isStateAddable;
+  const _Game_Battler_isStateAddable = Game_Battler.prototype.isStateAddable;
   Game_Battler.prototype.isStateAddable = function (stateId) {
     // 優先度の高い同グループステートにかかっている場合はそのステートにかからない
-    return _GameBattler_isStateAddable.call(this, stateId) &&
+    return _Game_Battler_isStateAddable.call(this, stateId) &&
       !this.isHighPriorityStateGroupAffected(stateId);
   };
 
@@ -53,25 +76,29 @@
    * 同じグループに属する優先度の高いステートにかかっているかどうか
    */
   Game_Battler.prototype.isHighPriorityStateGroupAffected = function (stateId) {
-    var state = $dataStates[stateId];
-    var sameGroupState = this.affectedSameGroupState(stateId);
-    return sameGroupState ? sameGroupState.statePriority > state.statePriority : false;
+    const priority = $dataStates[stateId].statePriority;
+    const highestPriority = this.affectedSameGroupStates(stateId).reduce((result, state) => Math.max(result, state.statePriority), priority);
+    return highestPriority > priority;
   };
 
   /**
    * 同じグループに属する優先度の低いステートにかかっているかどうか
    */
   Game_Battler.prototype.isLowPriorityStateGroupAffected = function (stateId) {
-    var state = $dataStates[stateId];
-    var sameGroupState = this.affectedSameGroupState(stateId);
-    return sameGroupState ? sameGroupState.statePriority < state.statePriority : false;
+    const priority = $dataStates[stateId].statePriority;
+    const lowestPriority = this.affectedSameGroupStates(stateId).reduce((result, state) => Math.min(result, state.statePriority), priority);
+    return lowestPriority < priority;
   };
 
   /**
    * かかっているステートの中で、同じグループに属するステートを取得する
+   * @return {RPG.State[]}
    */
-  Game_Battler.prototype.affectedSameGroupState = function (stateId) {
-    var state = $dataStates[stateId];
-    return this.states().find(activeState => activeState.stateGroup === state.stateGroup);
+  Game_Battler.prototype.affectedSameGroupStates = function (stateId) {
+    return this.states().filter(activeState =>
+      activeState.stateGroup.some(activeState =>
+        $dataStates[stateId].stateGroup.includes(activeState)
+      )
+    );
   };
 })();
